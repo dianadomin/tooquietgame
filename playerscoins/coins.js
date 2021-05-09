45086684bb24c9e7d02be3daebd0c61b4100c822
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-05-09 03:09:18 -0700

    Beautified the Code

--- playerscoins/coins.js
+++ playerscoins/coins.js
@@ -1,18 +1,18 @@
 class Coin {
-  constructor(){
+  constructor() {
     this.r = 50;
     this.x = random(w);
     this.y = 0 - this.r;
-    
+
   }
-  
-  display(){
+
+  display() {
     //ellipse(this.x, this.y, this.r, this.r);
-    image(coinImg,this.x, this.y, this.r, this.r);
+    image(coinImg, this.x, this.y, this.r, this.r);
   }
-  
-  move(){
+
+  move() {
     this.y++;
   }
-  
+
 }
